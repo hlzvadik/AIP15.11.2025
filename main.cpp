727ec9532fbf94d90cea5ba3a68f87967b734105
vadik <vadik@debian>
2025-11-15 12:54:22 +0300

    MUL

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 void sum(size_t n, size_t m, int * c, const int * a, const int * b);
 int * sum(size_t n, size_t m, const int * a, const int * b);
 void add(size_t n, size_t m, const int * a, const int * b);
+void mul(int * c, const int * a, size_t n1, size_t m1, const int * b, size_t m2);
 
 int main()
 {
@@ -15,7 +16,7 @@ void sum(size_t n, size_t m, int * c, const int * a, const int * b)
   {
     for (size_t j = 0; j < m; ++j)
     {
-      c[i][j] = a[i][j] + b[i][j];
+      c[i*m + j] = a[i*m + j] + b[i*m + j];
     }
   }
 }
@@ -26,7 +27,23 @@ int * sum(size_t n, size_t m, const int * a, const int * b)
   sum(n, m, c, a, b);
 }
 
-void add(size_t n, size_t m, const int * a, const int * b)
+void add(size_t n, size_t m, int * a, const int * b)
 {
   sum(n, m, a, a, b);
+}
+
+void mul(int * c, const int * a, size_t n1, size_t m1, const int * b, size_t m2)
+{
+  for (size_t i = 0; i < n1; ++i)
+  {
+    for (size_t j = 0;  j<m2; ++j)
+    {
+      int res = 0;
+      for (size_t k = 0; k < m1; ++k)
+      {
+        res += a[i*m1 + k]*b[k*m2 + j];
+      }
+      c[i*m2 + j] = res;
+    }
+  }
 }
