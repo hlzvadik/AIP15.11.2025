8f20856b12940666de7e916ee63980cc3a5636f9
vadik <vadik@debian>
2025-11-22 12:39:31 +0300

    Redact mistakes from last classWork

--- main.cpp
+++ main.cpp
@@ -1,140 +1,65 @@
 #include <iostream>
 
-void sum(size_t n, size_t m, int * c, const int * a, const int * b);
-int * sum(size_t n, size_t m, const int * a, const int * b);
-void add(size_t n, size_t m, const int * a, const int * b);
-void mul(int * c, const int * a, size_t n1, size_t m1, const int * b, size_t m2);
+struct Ints
+{
+  ~Ints();
+  Ints();
+  Ints(const Ints & w);
+  Ints(Ints && w);
+  Ints & operator=(const Ints & w);
+  Ints & operator=(Ints &&);
 
-void sum_of_mul(size_t n, int * c, const int * a, const int * const * b, size_t s);
-int * sum_of_mul(size_t n, const int * a, const int * const * b, size_t s);
-void mul_of_mul(size_t n, int * c, const int * a, const int * const * b, size_t s);
-int * mul_of_mul(size_t n, const int * a, const int * const * b, size_t s);
+private:
+  int* d;
+  size_t k;
+};
 
 int main()
 {
 
 }
 
-void sum(size_t n, size_t m, int * c, const int * a, const int * b)
+Ints::Ints():
+  d(nullptr),
+  k(0)
+{}
+
+Ints::Ints(const Ints & w):
+  d(w.k ? new int[w.k] : nullptr),
+  k(w.k)
 {
-  for (size_t i = 0; i < n; ++i)
+  for (size_t i = 0; i < w.k; ++ i)
   {
-    for (size_t j = 0; j < m; ++j)
+    d[i] = w.d[i];
+  }
+}
+
+Ints & Ints::operator=(const Ints & w)
+{
+  int * r = nullptr;
+  if (w.k)
+  {
+    r = new int[w.k];
+    for (size_t i = 0; i < w.k; ++ i)
     {
-      c[i * m + j] = a[i * m + j] + b[i * m + j];
+      d[i] = w.d[i];
     }
+    delete[] d;
+    d = r;
+    k = w.k;
+    return *this;
   }
 }
 
-int * sum(size_t n, size_t m, const int * a, const int * b)
+Ints & Ints::operator=(Ints && w)
 {
-  int * c = new int[n * m];
-  sum(n, m, c, a, b);
-}
-
-void add(size_t n, size_t m, int * a, const int * b)
-{
-  sum(n, m, a, a, b);
-}
-
-void mul(int * c, const int * a, size_t n1, size_t m1, const int * b, size_t m2)
-{
-  for (size_t i = 0; i < n1; ++i)
+  if (this == &w)
   {
-    for (size_t j = 0;  j < m2; ++j)
-    {
-      int res = 0;
-      for (size_t k = 0; k < m1; ++k)
-      {
-        res += a[i * m1 + k] * b[k * m2 + j];
-      }
-      c[i*m2 + j] = res;
-    }
+    return *this;
   }
-}
-
-void sum_of_mul(size_t n, int * c, const int * a, const int * const * b, size_t s)
-{
-  for (size_t i = 0; i < s; ++i)
-  {
-    c = sum(n, n, c, b[i]);
-  }
-  mul(c, a, n, n, c, n);
-}
-
-int * sum_of_mul(size_t n, const int * a, const int * const * b, size_t s)
-{
-  int * c = new int[n];
-  sum_of_mul(n, c, a, b, s);
-  return c;
-}
-
-void mul_of_mul(size_t n, int * c, const int * a, const int * const * b, size_t s)
-{
-  for (size_t i = 0; i < n; ++i)
-  {
-    for (size_t j = 0; j < n; ++j)
-    {
-      if (i == j)
-      {
-        c[i*n + j] = 1;
-      }
-      else
-      {
-        c[i*n + j] = 0;
-      }
-    }
-  }
-
-  for (size_t i = 0; i < s; ++i)
-  {
-    int * t = new int[n];
-    mul(t, a, n, n, b[i], n);
-    mul(c, t, n, n, c, n);
-    delete[] t;
-  }
-
-}
-
-int * mul_of_mul(size_t n, const int * a, const int * const * b, size_t s)
-{
-  int * c = new int[n];
-  mul_of_mul(n, c, a, b, s);
-  return c;
-}
-
-
-
-void mul_of_sum(size_t n, int * c, const int * a, const int * const * b, size_t s)
-{
-  for (size_t i = 0; i < n; ++i)
-  {
-    for (size_t j = 0; j < n; ++j)
-    {
-      if (i == j)
-      {
-        c[i*n + j] = 1;
-      }
-      else
-      {
-        c[i*n + j] = 0;
-      }
-    }
-  }
-
-  for (size_t i = 0; i < s; ++i)
-  {
-    int * t = new int[n];
-    sum(n, n, t, a, b[i]);
-    mul(c, t, n, n, c, n);
-    delete[] t;
-  }
-
-}
-
-int * mul_of_sum(size_t n, const int * a, const int * const * b, size_t s)
-{
-  int * c = new int[n];
-  mul_of_sum(n, c, a, b, s);
-  return c;
+  delete[] d;
+  d = w.d;
+  k = w.k;
+  w.d = nullptr;
+  return *this;
 }
