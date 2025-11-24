f4dd00c19af8fa807c513b43a8191a5de02ceacb
vadik <vadik@debian>
2025-11-24 16:41:04 +0300

    2tesk

--- main.cpp
+++ main.cpp
@@ -20,6 +20,28 @@ size_t count_year(const Stud * const * studs, size_t k, int y)
   return r;
 }
 
+const Stud ** stud_dep(const Stud * const * studs, size_t k, int dep, size_t & found)
+{
+  size_t dep_count = 0;
+  for (size_t i = 0; i < k; ++i)
+  {
+    const Stud * stud_i = studs[i];
+    dep_count += (stud_i->dep == dep);
+  }
+  const Stud ** r = new const Stud*[dep_count];
+  size_t id = 0;
+  for (size_t i = 0; i < k; ++i)
+  {
+    const Stud * stud_i = studs[i];
+    if (stud_i->dep == dep)
+    {
+      r[id] = stud_i;
+      ++id;
+    }
+  }
+  return r;
+}
+
 int main()
 {
 
