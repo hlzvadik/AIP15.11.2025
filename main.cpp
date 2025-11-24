523d5b7a5dbfb531dc7272dc24a9b9c264f40653
vadik <vadik@debian>
2025-11-24 17:07:20 +0300

    3,4task

--- main.cpp
+++ main.cpp
@@ -8,10 +8,28 @@ struct Stud
   int dep;
 };
 
+struct Subj
+{
+  const char * name;
+  size_t people, classes;
+  const Stud * const * studs;
+  const size_t * visited;
+};
+
+size_t count_year(const Stud * const * studs, size_t k, int y);
+const Stud ** stud_dep(const Stud * const * studs, size_t k, int dep, size_t & found);
+size_t max_visits(const Subj & sub, int y);
+bool is_better(const Subj& sub, int y1, int y2);
+
+int main()
+{
+
+}
+
 size_t count_year(const Stud * const * studs, size_t k, int y)
 {
   size_t r = 0;
-  for (size_t i = 0, i < k, ++i)
+  for (size_t i = 0; i < k; ++i)
   {
     const Stud * stud_i = studs[i];
     int stud_y = stud_i->year;
@@ -42,7 +60,31 @@ const Stud ** stud_dep(const Stud * const * studs, size_t k, int dep, size_t & f
   return r;
 }
 
-int main()
+size_t max_visits(const Subj & sub, int y)
 {
+  size_t cy = count_year(sub.studs, sub.people, y);
+  return cy * sub.classes;
+}
 
+size_t visits(const Subj & sub, int y)
+{
+  size_t r = 0;
+  for (size_t i = 0; i < sub.people; ++i)
+  {
+    const Stud*stud_i = sub.studs[i];
+    if (stud_i->year == y)
+    {
+      r += sub.visited[i];
+    }
+  }
+  return r;
+}
+
+bool is_better(const Subj& sub, int y1, int y2)
+{
+  size_t n_y1 = visits(sub, y1);
+  size_t q_y1 = max_visits(sub, y1);
+  size_t n_y2 = visits(sub, y2);
+  size_t q_y2 = max_visits(sub, y2);
+  return (n_y1 * q_y2 > n_y2 * q_y1);
 }
